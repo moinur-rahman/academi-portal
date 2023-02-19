54953a8c8ae84a43a86ed86fb2ea1c016c4e3cb4
Md. Maruful Islam <zxalex007@gmail.com>
2023-02-20 01:44:07 +0600

    student result page complete

--- lib/components/StudentResult/data.dart
+++ lib/components/StudentResult/data.dart
@@ -1,22 +1,368 @@
-const List<Map<String, String>> data = [
-  {
-    'courseCode': 'Phy-141',
-    'gpa': '3.25',
-  },
-  {
-    'courseCode': 'Hum-141',
-    'gpa': '3.25',
-  },
-  {
-    'courseCode': 'EE-181',
-    'gpa': '3.25',
-  },
-  {
-    'courseCode': 'Math-141',
-    'gpa': '3.25',
-  },
-  {
-    'courseCode': 'CSE-121',
-    'gpa': '3.25',
-  },
-];
+Map<String, List<Map<String, String>>> l1_t1 = {
+  'course': [
+    {
+      'courseCode': 'Phy-141',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-141',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-181',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-141',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-121',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-142',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-142',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-182',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-142',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-122',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l1_t2 = {
+  'course': [
+    {
+      'courseCode': 'Phy-241',
+      'gpa': '3.75',
+    },
+    {
+      'courseCode': 'Hum-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-281',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-221',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-242',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-242',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-282',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-242',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-222',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l2_t1 = {
+  'course': [
+    {
+      'courseCode': 'Phy-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-281',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-221',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-242',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-242',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-282',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-242',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-222',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l2_t2 = {
+  'course': [
+    {
+      'courseCode': 'Phy-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-281',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-241',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-221',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-242',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-242',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-282',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-242',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-222',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l3_t1 = {
+  'course': [
+    {
+      'courseCode': 'Phy-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-381',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-321',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-342',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-342',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-382',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-342',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-322',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l3_t2 = {
+  'course': [
+    {
+      'courseCode': 'Phy-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-381',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-341',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-321',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-342',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-342',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-382',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-342',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-322',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l4_t1 = {
+  'course': [
+    {
+      'courseCode': 'Phy-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-481',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-421',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-442',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-442',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-482',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-442',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-422',
+      'gpa': '4.00',
+    },
+  ]
+};
+Map<String, List<Map<String, String>>> l4_t2 = {
+  'course': [
+    {
+      'courseCode': 'Phy-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Hum-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'EE-481',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'Math-441',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-421',
+      'gpa': '3.25',
+    },
+  ],
+  'sessional': [
+    {
+      'courseCode': 'Phy-442',
+      'gpa': '3.50',
+    },
+    {
+      'courseCode': 'Hum-442',
+      'gpa': '3.00',
+    },
+    {
+      'courseCode': 'EE-482',
+      'gpa': '2.75',
+    },
+    {
+      'courseCode': 'Math-442',
+      'gpa': '3.25',
+    },
+    {
+      'courseCode': 'CSE-422',
+      'gpa': '4.00',
+    },
+  ]
+};
